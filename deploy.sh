dc37255f7e3ba0459ced63f5fbc4e684abef2ee0
Christopher Wetherill <chris@tbmh.org>
2020-01-11 18:32:56 -0500

    Config files

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+cp cowsay.service /usr/lib/systemd/system/cowsay.service
+systemctl daemon-reload
+systemctl restart cowsay
+
+cp cowsay.tbmh.org.conf /etc/nginx/conf.d/cowsay.tbmh.org.conf
+nginx reload
